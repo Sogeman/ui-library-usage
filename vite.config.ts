cd0ac6fa0c440caba4446ac04eaf0fef8530edc4
Rene Mandler <r.mandler@overlap.at>
2025-12-03 12:24:02 +0100

    feat: adds github deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ui-library-usage/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
